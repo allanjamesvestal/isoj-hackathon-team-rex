7192a2ef810a47f405445a582fc709c2d2411a94
Allan James Vestal <allanjamesvestal@gmail.com>
2017-04-20 17:07:36 -0500

    Made panels work.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,13 +6,3 @@ var d = new Date();
 var year = d.getFullYear();
 
 $('.copyright').text(year);
-
-
-// Open, close nav
-function openNav() {
-  document.getElementById("mySidenav").style.display = "block";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.display = "none";
-}
